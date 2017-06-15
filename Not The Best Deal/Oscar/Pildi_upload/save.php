b20b4a83c2008867e01c9ced225cb585d0a2115e
Oscar August heinla <oaheinla@tlu.ee>
2017-06-15 11:03:05 +0300

    Pildi salvestamise juppi lisamine

--- Not The Best Deal/Oscar/Pildi_upload/save.php
+++ Not The Best Deal/Oscar/Pildi_upload/save.php
@@ -1,4 +1,5 @@
 <?php
+$piclocation = "http://greeny.cs.tlu.ee/~oaheinla/Praktika/Veebirakendus-_yldhariduskoolide_v6rk/Not%20The%20Best%20Deal/Oscar/Pildi_upload/";
 $target_dir = "upload/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -7,38 +8,39 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        echo "fail on pilt - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "fail ei ole pilt.";
         $uploadOk = 0;
     }
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "Sellise nimegas fail on juba olemas.";
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+    echo "Pilt on liiga suur.";
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo "vale pildiformaat, lubatud on JPG, JPEG ja PNG formaadid.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    echo "Pildi laadimine ei õnnestunud.";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        
+        echo "Pilt nimega ". basename( $_FILES["fileToUpload"]["name"]). " salvestamine õnnestus.<br>".$piclocation.$target_file;
+                
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "Pildi laadimine ei õnnestunud.";
     }
 }
 ?>
