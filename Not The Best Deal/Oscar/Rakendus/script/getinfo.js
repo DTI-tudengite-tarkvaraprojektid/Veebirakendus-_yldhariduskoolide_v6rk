181bbbb6f56b07674e8c5d09402670e833627781
Oscar August heinla <oaheinla@tlu.ee>
2017-06-16 15:29:57 +0300

    uuendused

---
+++ Not The Best Deal/Oscar/Rakendus/script/getinfo.js
@@ -0,0 +1,217 @@
+var schools;
+var schoolname;
+var geocoder;
+var regID;
+var people;
+var startYear;
+var principals;
+
+window.onload = initMap();
+
+        function initMap() {
+                schoolname = location.search.substring(12);
+                console.log(schoolname);
+                getRegID();
+        }
+        function getRegID(){
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                        regID = JSON.parse(xhttp.responseText);
+                        console.log(JSON.parse(xhttp.responseText));
+                        console.log('loaded');
+                        getSchools();
+                }
+            };
+            xhttp.open("GET", '../Php/getregnr.php?school='+schoolname, true);
+            xhttp.send();
+                
+        }
+
+        //Võtab andmebaasist kooli andmed
+        function getSchools(){
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    schools = JSON.parse(xhttp.responseText);
+                    console.log(JSON.parse(xhttp.responseText));
+                    console.log('loaded');
+                    addinfo();
+                    //loadMap();
+                    
+                }
+            };
+            xhttp.open("GET", '../Php/getinfo.php?REG_ID='+regID, true);
+            xhttp.send();
+        }
+
+        //Kuvab kooli nime ja muu ingo
+        function addinfo(){
+            var name = schools[0].name;
+            var website =  schools[0].webpage;
+            var address = schools[0].county+", "+schools[0].parish+", "+schools[0].address;
+            var openyear = schools[0].openyear;
+            if(name === "DEFAULT"){
+                name = "---";
+            }
+            if(website === "DEFAULT"){
+                website = "---";
+            }
+            if(address === "DEFAULT"){
+                address = "---";
+            }
+            document.getElementById("name").innerHTML = name;
+            //console.log(schools[0].webpage);
+            document.getElementById("website").innerHTML = website;
+            document.getElementById("address").innerHTML = address;
+            document.getElementById("openyear").innerHTML = openyear;
+            loadMap();
+        }
+
+        //Laeb googlemapsi kaardi
+        function loadMap(){
+            
+            //proovib leida aadressi kauadu asukohta
+            var countysql = schools[0].county;
+            var parishsql = schools[0].parish;
+            var aadresssql = schools[0].address;
+        
+
+            geocoder = new google.maps.Geocoder();
+            aadress =  decodeURIComponent(schoolname);
+            
+            console.log(aadress);
+            geocoder.geocode( { 'address': aadress}, function(results, status) {
+
+                if (status == google.maps.GeocoderStatus.OK) {
+                    latitude = results[0].geometry.location.lat();
+                    longitude = results[0].geometry.location.lng();
+                    console.log("lat: "+latitude+" long: "+longitude);
+
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 17,
+                        center: {lat: latitude, lng: longitude}
+                    });
+                    
+                    var marker = new google.maps.Marker({
+                        position: {lat: latitude, lng: longitude},
+                        map: map
+                    });
+                //kui aadressi kaudu ei suutnud koordinaate leida siis proovib leida nime järgi 
+                } else {
+                    console.log("uus katse");
+                    //alert('Geocode was not successful for the following reason: ' + status);
+                    aadress = countysql+", "+parishsql+", "+aadresssql;
+                    makeMarker();
+                }
+            });
+    getprincipals();   
+    }
+    
+    //Nime järgi asukoha leidmise funktsioon
+    function makeMarker (){
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode( { 'address': aadress}, function(results, status) {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+                latitude = results[0].geometry.location.lat();
+                longitude = results[0].geometry.location.lng();
+                console.log("lat: "+latitude+" long: "+longitude);
+
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 17,
+                    center: {lat: latitude, lng: longitude}
+                });
+                
+                var marker = new google.maps.Marker({
+                    position: {lat: latitude, lng: longitude},
+                    map: map
+                });
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+    }
+
+    function getPeople(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+                people = JSON.parse(xhttp.responseText);
+                console.log(JSON.parse(xhttp.responseText));
+                console.log('loaded');
+                startYear = people[0]; 
+                people.shift();
+                console.log(people);  
+                loadDia();
+        }
+     };
+        xhttp.open("GET", '../Php/getpeopleinfo.php?regid='+regID, true);
+        xhttp.send();
+            
+    }
+
+    function loadDia(){
+        Highcharts.chart('container1', {
+
+        title: {
+            text: schoolname,
+        },
+
+        /*subtitle: {
+            text: 'Source: thesolarfoundation.com'
+        },*/
+
+        yAxis: { 
+            title: {
+                text: 'Õpilaste arv',
+                
+            }
+        },
+        /*legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },*/
+
+        plotOptions: {
+            series: {
+                pointStart: startYear
+            }
+        },
+
+        series: [{
+            name: "Õpilaste arv",
+            color: '#b71234',
+            data: people
+        }, ]
+
+    });
+}
+function getprincipals(){
+                  var xhttp = new XMLHttpRequest();
+                  xhttp.onreadystatechange = function() {
+                      if (this.readyState == 4 && this.status == 200) {
+                              principals = JSON.parse(xhttp.responseText);
+                              console.log("tired: "+JSON.parse(xhttp.responseText));
+                              console.log('loaded');    
+                              print();   
+                      }
+                  };
+                  xhttp.open("GET", '../Php/getprincipals.php?REG_ID='+regID, true);
+                  xhttp.send();
+
+                      
+      }
+    function print(){
+        for (i = 0; i < principals.length; i++) { 
+                  var ul = document.getElementById("principals");
+                  var li = document.createElement("li");
+                  var children = ul.children.length + 1
+                  li.setAttribute("id", "element"+children)
+                  var principal = principals[i].principal+" "+principals[i].start+"-"+principals[i].end;
+                  li.appendChild(document.createTextNode(principal));
+                  ul.appendChild(li);
+        }
+    getPeople();
+    }
